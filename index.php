868ec77eb915f4ee17464e552d47d5be213e4430
mrthota <31176003+mrthota@users.noreply.github.com>
2019-01-15 17:07:47 -0500

    Update index.php
    Removed the PR.

--- index.php
+++ index.php
@@ -15,25 +15,17 @@ $opt = '/_cat/health?h=status';
 // CSS Styles
 echo "<style> .blue {   background: blue; } </style>";
 
-$llc_servers = array(
+$es_llc_servers = array(
   array('QA', 'cmftes00.homedepot.com:9200', 'e5admin'),
   array('Q1', 'cmf1es00.homedepot.com:9200', 'e5admin'),
   array('Q2', 'cmf2es00.homedepot.com:9200', 'e561admin'),
   array('Q3', 'cmf3es00.homedepot.com:9200', 'e5admin'),
-  array('QP-A', 'ccliqpe0.homedepot.com:9200', 'e5admin'),
-  array('QP-S', 'ccliqpf0.homedepot.com:9200', 'e5admin'),
-  array('PR-A', 'cclidie0.homedepot.com:9200', 'E559Adm19n'),
-  array('PR-S', 'cclidif0.homedepot.com:9200', 'e559adm1n'),
   //array('Local', 'host.docker.internal:9200', 9200,'e5admin'),
   //array('Local', 'localhost:9200', 9200,'e5admin'),
-  //array('Local', 'localhost:9200', 9200,'e5admin'),
-  //array('Local', 'localhost:9200', 9200,'e5admin'),
-  //array('Local', 'localhost:9200', 9200,'e5admin'),
-  //array('Local', 'localhost:9200', 9200,'e5admin'),
 );
 
 
-$qp_servers = array(
+$es_qp_servers = array(
   array('QP-A', 'ccliqpe0.homedepot.com:9200', 'e5admin'),
   array('QP-S', 'ccliqpf0.homedepot.com:9200', 'e5admin'),
   array('Local', 'localhost:9200', 9200,'e5admin'),
@@ -44,15 +36,10 @@ $qp_servers = array(
   array('Local', 'localhost:9200', 9200,'e5admin'),
 );
 
-
+// Elastic PR Servers (Not using inside)
 $pr_servers = array(
   array('PR-A', 'cclidie0.homedepot.com:9200', 'E559Adm19n'),
   array('PR-S', 'cclidif0.homedepot.com:9200', 'e559adm1n'),
-  array('Local', 'localhost:9200', 9200,'e5admin'),
-  array('Local', 'localhost:9200', 9200,'e5admin'),
-  array('Local', 'localhost:9200', 9200,'e5admin'),
-  array('Local', 'localhost:9200', 9200,'e5admin'),
-  array('Local', 'localhost:9200', 9200,'e5admin'),
   array('Local', 'localhost:9200', 9200,'e5admin'),
 
 );
@@ -61,33 +48,30 @@ $redis_servers = array(
   array('QA', 'ccliqae1.homedepot.com', 6379),
   array('QP-A', 'com-redis-sa-qp-atc.homedepot.com', 10001),
   array('QP-S', 'com-redis-sa-qp-ssc.homedepot.com', 10001),
-  array('PR-A', 'com-redis-sa-pr-atc.homedepot.com', 10001),
-  array('PR-S', 'com-redis-sa-pr-ssc.homedepot.com', 10001),
+  //array('PR-A', 'com-redis-sa-pr-atc.homedepot.com', 10001),
+  //array('PR-S', 'com-redis-sa-pr-ssc.homedepot.com', 10001),
 
 );
 
-//echo '<body link="grey">';
-//border="0" cellspacing="0" cellpadding="4"
-//echo '<table style="width:100%" border="1" cellspacing="4" cellpadding="4" >';
+
 echo '<table border="5" border="1" frame="void" rules="border" style="width:50%" cellpadding="2" cellspacing="4">';
 echo '<CAPTION><font size=6>Service Availability Dashboard</font></CAPTION>';
+
+// --- Elastic
 echo '    <tr><th colspan="100%" ><font size=6>Elastic</font></th></tr>';
 
-// --- LLC Servers
+// --- Elastic LLC Servers
 echo '<tr>';
 echo '<td bgcolor="orange" ><font face="verdana" size=5 >LLC</font></td>';
-foreach ($llc_servers as list($a, $b, $c))
+foreach ($es_llc_servers as list($a, $b, $c))
 {
 
 $curl = curl_init();
-// Set some options - we are passing in a useragent too here
 
 curl_setopt_array($curl, array(
     CURLOPT_RETURNTRANSFER => 1,
     CURLOPT_URL => $b.$opt,
     CURLOPT_HTTPAUTH => CURLAUTH_ANY,
-//CURLOPT_USERPWD  => "$username:$password",
-//CURLOPT_RETURNTRANSFER   => true,
     CURLOPT_USERPWD  => "es_admin:".$c
 ));
 // Send the request & save response to $resp
@@ -95,12 +79,10 @@ $resp = curl_exec($curl);
 
 if ( !empty($resp) )
  {
-//echo '<td><font color=#e40000>'.$resp.'</font></td>';
 echo '<td align="center" bgcolor='.$resp.'><font face="verdana" size=3 >'.$a.'</font></td>';
  }
 else
  {
-//echo '<td><font face="verdana" size=5 color=grey> <a color=grey href= '. curl_error($curl) '>'.$a.'</a></font></td>';
 echo '<td align="center"><font face="verdana" size=3 color=grey>'.$a.'</font></td>';
  }
 curl_close($curl);
@@ -108,10 +90,10 @@ curl_close($curl);
 }
 echo '</tr>';
 
-// --- QP Servers
+// --- Elastic QP Servers
 echo '<tr>';
 echo '<td class="blue" ><font face="verdana" size=5 >QP</font></td>';
-foreach ($qp_servers as list($a, $b, $c))
+foreach ($es_qp_servers as list($a, $b, $c))
 {
 
 $curl = curl_init();
@@ -138,58 +120,16 @@ curl_close($curl);
 }
 echo '</tr>';
 
-// --- PROD Servers
-echo '    <tr><th colspan="100%" bgcolor="grey" ><font size=6></font></th></tr>';
-echo '    <tr><th colspan="100%" ><font size=6>Cassandra</font></th></tr>';
-echo '<tr>';
-echo '<td bgcolor="orange" ><font face="verdana" size=5 >QP</font></td>';
-foreach ($pr_servers as list($a, $b, $c))
-{
-
-$curl = curl_init();
-// Set some options - we are passing in a useragent too here
-
-curl_setopt_array($curl, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $b.$opt,
-    CURLOPT_HTTPAUTH => CURLAUTH_ANY,
-    CURLOPT_USERPWD  => "es_admin:".$c
-));
-// Send the request & save response to $resp
-$resp = curl_exec($curl);
-
-        // Check if any error has occurred
-//        if (curl_errno($curl))
-//        {
-//            $ela_error = curl_error($curl);
-//        }
-//        else
-//        {
-            // cURL executed successfully
-//            print_r(curl_getinfo($curl));
-//        }
-
-if ( !empty($resp) )
- {
-echo '<td align="center" bgcolor='.$resp.'><font face="verdana" size=3 >'.$a.'</font></td>';
- }
-else
- {
-echo '<td align="center" bgcolor="grey"><font face="verdana" size=3 >'.$a.'</font><details Hide><summary><font size=1>>>></summary><p>'.curl_error($curl).'</font></p></details></td>';
- }
-curl_close($curl);
-
-}
 
 
-
-echo '</tr>';
-
-// --- PROD Servers
+// --- REDIS
 echo '    <tr><th colspan="100%" bgcolor="brown" ><font size=6></font></th></tr>';
 echo '    <tr><th colspan="100%" ><font size=6>Redis</font></th></tr>';
+
+// --- Redis LLC Servers
 echo '<tr>';
-echo '<td bgcolor="orange" ><font face="verdana" size=5 >QA</font></td>';
+echo '<td bgcolor="orange" ><font face="verdana" size=5 >LLC</font></td>';
+
 foreach ($redis_servers as list($a, $b, $c))
 {
 
@@ -229,21 +169,11 @@ $redis=new Redis();
     }
 
 }
-
-// Loop should go above this point
-
 echo '</tr>';
-echo '</table>';
 
-        // Check if any error has occurred
-//        if (curl_errno($curl))
-//        {
-//            echo 'cURL error: ' . curl_error($curl);
-//        }
-//        else
-//        {
-            // cURL executed successfully
-//            print_r(curl_getinfo($curl));
-//        }
+// Loop should go above this point and between <tr>
+
+
+echo '</table>';
 
 ?>
